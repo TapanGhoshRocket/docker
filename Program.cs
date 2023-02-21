64a74a32ea98128a185a12d6fdfd2a319df74434
Devendra-Singh-Bhadauria <125956926+Devendra-Singh-Bhadauria@users.noreply.github.com>
2023-02-21 13:15:20 +0530

    Update Program.cs
    Checking as a collaborator edit rights.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using IBM.Data.Db2;
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!!!");
 Console.WriteLine("Using DB2 .NET provider");
 string connString = "Database=sample;UserID=newton;Server=Waldevdbclnxtst06.dev.rocketsoftware.com:60000;pwd=A2m8test;";
 DB2Connection con1 = new DB2Connection(connString);
